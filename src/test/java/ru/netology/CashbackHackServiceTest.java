4465ed727e0b2a531b78a4f56d34bf5a4c90253e
Khazova Lyubov <lyubashilina@rambler.ru>
2020-11-27 15:54:07 +0400

    Added testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void quantityUpTo1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void quantityMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void quantityWhenEqualTo1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
